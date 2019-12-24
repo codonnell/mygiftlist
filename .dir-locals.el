28b7a706be5d3f7b1766b3baa427906b5e1b6c5d
Chris O'Donnell <chris.bowdoin@gmail.com>
2019-12-24 00:02:20 -0500

    Add dir-locals.

---
+++ .dir-locals.el
@@ -0,0 +1,4 @@
+((nil . ((cider-clojure-cli-global-options . "-A:dev")
+         (cider-default-clj-repl . clojure-cli)
+         (cider-default-cljs-repl . shadow)
+         (cider-shadow-default-options . ":dev"))))
