6956eb18ec535e108ae6f72d837c909fc488bfb3
Chris O'Donnell <chris.bowdoin@gmail.com>
2019-12-25 14:33:22 -0500

    Convert user resolvers to batch resolvers.

--- src/rocks/mygiftlist/model/user.clj
+++ src/rocks/mygiftlist/model/user.clj
@@ -13,84 +13,109 @@
                        :where [[_ ::id ?id]]})
                 (mapv (fn [[id]] {::id id})))})
 
-(defresolver user-by-id-resolver [{:keys [db]} {::keys [id]}]
+(defresolver user-by-id-resolver [{:keys [db]} inputs]
   {::pc/input #{::id}
-   ::pc/output [::auth0-id]}
-  (ffirst (d/q '{:find [(pull ?u [::auth0-id])]
-                 :in [$ ?id]
-                 :where [[?u ::id ?id]]}
-            db id)))
+   ::pc/output [::auth0-id]
+   ::pc/transform pc/transform-batch-resolver}
+  (->> inputs
+    (mapv ::id)
+    (d/q '{:find [(pull ?u [::id ::auth0-id])]
+           :in [$ [?id ...]]
+           :where [[?u ::id ?id]]}
+      db)
+    (mapv first)
+    (pc/batch-restore-sort {::pc/inputs inputs ::pc/key ::id})))
 
-(defresolver user-by-auth0-id-resolver [{:keys [db]} {::keys [auth0-id]}]
+(defresolver user-by-auth0-id-resolver [{:keys [db]} inputs]
   {::pc/input #{::auth0-id}
-   ::pc/output [::id]}
-  (ffirst (d/q '{:find [(pull ?u [::id])]
-                 :in [$ ?auth0-id]
-                 :where [[?u ::auth0-id ?auth0-id]]}
-            db auth0-id)))
+   ::pc/output [::id]
+   ::pc/transform pc/transform-batch-resolver}
+  (->> inputs
+    (mapv ::auth0-id)
+    (d/q '{:find [(pull ?u [::auth0-id ::id])]
+           :in [$ [?auth0-id ...]]
+           :where [[?u ::auth0-id ?auth0-id]]}
+      db)
+    (mapv first)
+    (pc/batch-restore-sort {::pc/inputs inputs ::pc/key ::auth0-id})))
 
-(defresolver user-by-email-resolver [{:keys [db requester-auth0-id]} {::keys [email]}]
+(defresolver user-by-email-resolver [{:keys [db requester-auth0-id]} inputs]
   {::pc/input #{::email}
-   ::pc/output [::id ::auth0-id]}
-  (ffirst (d/q '{:find [(pull ?u [::id ::auth0-id])]
-                 :in [$ ?requester-auth0-id ?email]
-                 :where [[?u ::email ?email]
-                         (or-join [?u ?requester-auth0-id]
-                           [?u ::auth0-id ?requester-auth0-id]
-                           (and
-                             [?gift-list ::gift-list/created-by ?u]
-                             [?invitation ::invitation/gift-list ?gift-list]
-                             [?invitation ::invitation/accepted-by ?accepter]
-                             [?accepter ::auth0-id ?requester-auth0-id])
-                           (and
-                             [?list-owner ::auth0-id ?requester-auth0-id]
-                             [?gift-list ::gift-list/created-by ?list-owner]
-                             [?invitation ::invitation/gift-list ?gift-list]
-                             [?invitation ::invitation/accepted-by ?u]))]}
-            db requester-auth0-id email)))
+   ::pc/output [::id ::auth0-id]
+   ::pc/transform pc/transform-batch-resolver}
+  (->> inputs
+    (mapv ::email)
+    (d/q '{:find [(pull ?u [::email ::id ::auth0-id])]
+           :in [$ ?requester-auth0-id [?email ...]]
+           :where [[?u ::email ?email]
+                   (or-join [?u ?requester-auth0-id]
+                     [?u ::auth0-id ?requester-auth0-id]
+                     (and
+                       [?gift-list ::gift-list/created-by ?u]
+                       [?invitation ::invitation/gift-list ?gift-list]
+                       [?invitation ::invitation/accepted-by ?accepter]
+                       [?accepter ::auth0-id ?requester-auth0-id])
+                     (and
+                       [?list-owner ::auth0-id ?requester-auth0-id]
+                       [?gift-list ::gift-list/created-by ?list-owner]
+                       [?invitation ::invitation/gift-list ?gift-list]
+                       [?invitation ::invitation/accepted-by ?u]))]}
+      db requester-auth0-id)
+    (mapv first)
+    (pc/batch-restore-sort {::pc/inputs inputs ::pc/key ::email})))
 
-(defresolver user-email-resolver [{:keys [db requester-auth0-id]} {::keys [id]}]
+(defresolver user-email-resolver [{:keys [db requester-auth0-id]} inputs]
   {::pc/input #{::id}
-   ::pc/output [::email]}
-  (ffirst (d/q '{:find [(pull ?u [::email])]
-                 :in [$ ?requester-auth0-id ?id]
-                 :where [(or-join [?u ?id ?requester-auth0-id]
-                           (and
-                             [?u ::auth0-id ?requester-auth0-id]
-                             [?u ::id ?id])
-                           (and
-                             [?gift-list ::gift-list/created-by ?u]
-                             [?invitation ::invitation/gift-list ?gift-list]
-                             [?invitation ::invitation/accepted-by ?accepter]
-                             [?accepter ::auth0-id ?requester-auth0-id])
-                           (and
-                             [?list-owner ::auth0-id ?requester-auth0-id]
-                             [?gift-list ::gift-list/created-by ?list-owner]
-                             [?invitation ::invitation/gift-list ?gift-list]
-                             [?invitation ::invitation/accepted-by ?u]))]}
-            db requester-auth0-id id)))
+   ::pc/output [::email]
+   ::pc/transform pc/transform-batch-resolver}
+  (->> inputs
+    (mapv ::id)
+    (d/q '{:find [(pull ?u [::id ::email])]
+           :in [$ ?requester-auth0-id [?id ...]]
+           :where [(or-join [?u ?id ?requester-auth0-id]
+                     (and
+                       [?u ::auth0-id ?requester-auth0-id]
+                       [?u ::id ?id])
+                     (and
+                       [?gift-list ::gift-list/created-by ?u]
+                       [?invitation ::invitation/gift-list ?gift-list]
+                       [?invitation ::invitation/accepted-by ?accepter]
+                       [?accepter ::auth0-id ?requester-auth0-id])
+                     (and
+                       [?list-owner ::auth0-id ?requester-auth0-id]
+                       [?gift-list ::gift-list/created-by ?list-owner]
+                       [?invitation ::invitation/gift-list ?gift-list]
+                       [?invitation ::invitation/accepted-by ?u]))]}
+      db requester-auth0-id)
+    (mapv first)
+    (pc/batch-restore-sort {::pc/inputs inputs ::pc/key ::id})))
 
-(defresolver user-personal-data-resolver [{:keys [db requester-auth0-id]} {::keys [id]}]
+(defresolver user-personal-data-resolver [{:keys [db requester-auth0-id]} inputs]
   {::pc/input #{::id}
-   ::pc/output [::given-name ::family-name]}
-  (ffirst (d/q '{:find [(pull ?u [::given-name ::family-name])]
-                 :in [$ ?requester-auth0-id ?id]
-                 :where [[?u ::id ?id]
-                         (or-join [?u ?requester-auth0-id]
-                           [?u ::auth0-id ?requester-auth0-id]
-                           (and
-                             [?u ::display-name true]
-                             [?gift-list ::gift-list/created-by ?u]
-                             [?invitation ::invitation/gift-list ?gift-list]
-                             [?invitation ::invitation/accepted-by ?accepter]
-                             [?accepter ::auth0-id ?requester-auth0-id])
-                           (and
-                             [?u ::display-name true]
-                             [?list-owner ::auth0-id ?requester-auth0-id]
-                             [?gift-list ::gift-list/created-by ?list-owner]
-                             [?invitation ::invitation/gift-list ?gift-list]
-                             [?invitation ::invitation/accepted-by ?u]))]}
-            db requester-auth0-id id)))
+   ::pc/output [::given-name ::family-name]
+   ::pc/transform pc/transform-batch-resolver}
+  (->> inputs
+    (mapv ::id)
+    (d/q '{:find [(pull ?u [::id ::given-name ::family-name])]
+           :in [$ ?requester-auth0-id [?id ...]]
+           :where [[?u ::id ?id]
+                   (or-join [?u ?requester-auth0-id]
+                     [?u ::auth0-id ?requester-auth0-id]
+                     (and
+                       [?u ::display-name true]
+                       [?gift-list ::gift-list/created-by ?u]
+                       [?invitation ::invitation/gift-list ?gift-list]
+                       [?invitation ::invitation/accepted-by ?accepter]
+                       [?accepter ::auth0-id ?requester-auth0-id])
+                     (and
+                       [?u ::display-name true]
+                       [?list-owner ::auth0-id ?requester-auth0-id]
+                       [?gift-list ::gift-list/created-by ?list-owner]
+                       [?invitation ::invitation/gift-list ?gift-list]
+                       [?invitation ::invitation/accepted-by ?u]))]}
+      db requester-auth0-id)
+    (mapv first)
+    (pc/batch-restore-sort {::pc/inputs inputs ::pc/key ::id})))
 
 (def user-resolvers
   [all-users-resolver
