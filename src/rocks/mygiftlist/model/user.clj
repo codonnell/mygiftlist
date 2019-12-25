b535bd62ecda213dc59d16d10546c5090d885182
Chris O'Donnell <chris.bowdoin@gmail.com>
2019-12-25 14:55:02 -0500

    Add syntactic sugar for common batch query operation.

--- src/rocks/mygiftlist/model/user.clj
+++ src/rocks/mygiftlist/model/user.clj
@@ -2,6 +2,7 @@
   (:require
    [datomic.client.api :as d]
    [com.wsscode.pathom.connect :as pc :refer [defresolver defmutation]]
+   [rocks.mygiftlist.ion.query :as query]
    [rocks.mygiftlist.model.gift-list :as gift-list]
    [rocks.mygiftlist.model.gift-list.invitation :as invitation]))
 
@@ -17,34 +18,29 @@
   {::pc/input #{::id}
    ::pc/output [::auth0-id]
    ::pc/transform pc/transform-batch-resolver}
-  (->> inputs
-    (mapv ::id)
+  (query/batch-query-by ::id
     (d/q '{:find [(pull ?u [::id ::auth0-id])]
            :in [$ [?id ...]]
            :where [[?u ::id ?id]]}
       db)
-    (mapv first)
-    (pc/batch-restore-sort {::pc/inputs inputs ::pc/key ::id})))
+    inputs))
 
 (defresolver user-by-auth0-id-resolver [{:keys [db]} inputs]
   {::pc/input #{::auth0-id}
    ::pc/output [::id]
    ::pc/transform pc/transform-batch-resolver}
-  (->> inputs
-    (mapv ::auth0-id)
+  (query/batch-query-by ::auth0-id
     (d/q '{:find [(pull ?u [::auth0-id ::id])]
            :in [$ [?auth0-id ...]]
            :where [[?u ::auth0-id ?auth0-id]]}
       db)
-    (mapv first)
-    (pc/batch-restore-sort {::pc/inputs inputs ::pc/key ::auth0-id})))
+    inputs))
 
 (defresolver user-by-email-resolver [{:keys [db requester-auth0-id]} inputs]
   {::pc/input #{::email}
    ::pc/output [::id ::auth0-id]
    ::pc/transform pc/transform-batch-resolver}
-  (->> inputs
-    (mapv ::email)
+  (query/batch-query-by ::email
     (d/q '{:find [(pull ?u [::email ::id ::auth0-id])]
            :in [$ ?requester-auth0-id [?email ...]]
            :where [[?u ::email ?email]
@@ -61,15 +57,13 @@
                        [?invitation ::invitation/gift-list ?gift-list]
                        [?invitation ::invitation/accepted-by ?u]))]}
       db requester-auth0-id)
-    (mapv first)
-    (pc/batch-restore-sort {::pc/inputs inputs ::pc/key ::email})))
+    inputs))
 
 (defresolver user-email-resolver [{:keys [db requester-auth0-id]} inputs]
   {::pc/input #{::id}
    ::pc/output [::email]
    ::pc/transform pc/transform-batch-resolver}
-  (->> inputs
-    (mapv ::id)
+  (query/batch-query-by ::id
     (d/q '{:find [(pull ?u [::id ::email])]
            :in [$ ?requester-auth0-id [?id ...]]
            :where [(or-join [?u ?id ?requester-auth0-id]
@@ -87,15 +81,13 @@
                        [?invitation ::invitation/gift-list ?gift-list]
                        [?invitation ::invitation/accepted-by ?u]))]}
       db requester-auth0-id)
-    (mapv first)
-    (pc/batch-restore-sort {::pc/inputs inputs ::pc/key ::id})))
+    inputs))
 
 (defresolver user-personal-data-resolver [{:keys [db requester-auth0-id]} inputs]
   {::pc/input #{::id}
    ::pc/output [::given-name ::family-name]
    ::pc/transform pc/transform-batch-resolver}
-  (->> inputs
-    (mapv ::id)
+  (query/batch-query-by ::id
     (d/q '{:find [(pull ?u [::id ::given-name ::family-name])]
            :in [$ ?requester-auth0-id [?id ...]]
            :where [[?u ::id ?id]
@@ -114,8 +106,7 @@
                        [?invitation ::invitation/gift-list ?gift-list]
                        [?invitation ::invitation/accepted-by ?u]))]}
       db requester-auth0-id)
-    (mapv first)
-    (pc/batch-restore-sort {::pc/inputs inputs ::pc/key ::id})))
+    inputs))
 
 (def user-resolvers
   [all-users-resolver
