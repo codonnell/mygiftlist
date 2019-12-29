3ad29eb879171bb645eabfd3f597f9a38eaa2713
Chris O'Donnell <chris.bowdoin@gmail.com>
2019-12-29 10:45:58 -0500

    Write created-gift-list resolver.

--- src/rocks/mygiftlist/model/gift_list.clj
+++ src/rocks/mygiftlist/model/gift_list.clj
@@ -19,17 +19,14 @@
                 {::gift-list/gifts [::gift/id]}]
    ::pc/transform pc/transform-batch-resolver}
   (let [ids (mapv ::gift-list/id inputs)
-        raw-results (jdbc/execute! db/pool
-                      (sql/format
-                        {:select [:gl.id :gl.name :gl.created_at :u.id
-                                  [(sql/call :array_agg :g.id) :gift_ids]]
-                         :from [[:gift_list :gl]]
-                         :left-join [[:gift :g] [:= :g.gift_list_id :gl.id]
-                                     [:user :u] [:= :u.id :gl.created_by_id]]
-                         :where [:in :gl.id ids]
-                         :group-by [:gl.id :gl.name :gl.created_at :gl.created_by_id :u.id]}
-                        :quoting :ansi)
-                      db/query-opts)
+        raw-results (db/execute! db/pool
+                      {:select [:gl.id :gl.name :gl.created_at :u.id
+                                [(sql/call :array_agg :g.id) :gift_ids]]
+                       :from [[:gift_list :gl]]
+                       :left-join [[:gift :g] [:= :g.gift_list_id :gl.id]
+                                   [:user :u] [:= :u.id :gl.created_by_id]]
+                       :where [:in :gl.id ids]
+                       :group-by [:gl.id :gl.name :gl.created_at :gl.created_by_id :u.id]})
         results (mapv (fn [{:keys [gift-ids] :as gift-list}]
                         (-> gift-list
                           (assoc ::gift-list/gifts
@@ -38,40 +35,16 @@
                             (::user/id gift-list))
                           (dissoc :gift-ids ::user/id)))
                   raw-results)]
-    (println {:raw-results raw-results
-              :results results})
     (pc/batch-restore-sort {::pc/inputs inputs ::pc/key ::gift-list/id} results)))
 
-;; (defresolver gift-list-by-id-resolver [{:keys [db requester-auth0-id]} inputs]
-;;   {::pc/input #{::gift-list/id}
-;;    ::pc/output [::gift-list/name ::gift-list/created-at ::gift-list/created-by
-;;                 {::gift-list/gifts [::gift/id]}]
-;;    ::pc/transform pc/transform-batch-resolver}
-;;   (query/batch-query-by ::gift-list/id
-;;     (d/q '{:find [(pull ?gift-list [::gift-list/id ::gift-list/name
-;;                                ::gift-list/created-at ::gift-list/created-by
-;;                                {::gift-list/gifts [::gift/id]}])]
-;;            :in [$ ?requester-auth0-id [?id ...]]
-;;            :where [[?requester ::user/auth0-id ?requester-auth0-id]
-;;                    [?gift-list ::gift-list/id ?id]
-;;                    (or-join [?gift-list ?requester]
-;;                      [?gift-list ::gift-list/created-by ?requester]
-;;                      (and
-;;                        [?invitation ::invitation/gift-list ?gift-list]
-;;                        [?invitation ::invitation/accepted-by ?requester]
-;;                        (not-join [?gift-list]
-;;                          [?revocation ::revocation/gift-list ?gift-list]
-;;                          [?revocation ::revocation/user ?requester])))]}
-;;       db requester-auth0-id)
-;;     inputs))
-
-;; (defresolver created-gift-lists-resolver [{:keys [db requester-auth0-id]} _]
-;;   {::pc/output [{:created-gift-lists [::gift-list/id]}]}
-;;   {:created-gift-lists (mapv first (d/q '{:find [(pull ?gift-list [::gift-list/id])]
-;;                                           :in [$ ?requester-auth0-id]
-;;                                           :where [[?u ::user/auth0-id ?requester-auth0-id]
-;;                                                   [?gift-list ::gift-list/created-by ?u]]}
-;;                                      db requester-auth0-id))})
+(defresolver created-gift-lists-resolver [{::db/keys [pool] :keys [requester-auth0-id]} _]
+  {::pc/output [{:created-gift-lists [::gift-list/id]}]}
+  {:created-gift-lists
+   (db/execute! db/pool
+     {:select [:gl.id]
+      :from [[:gift_list :gl]]
+      :join [[:user :u] [:= :u.id :gl.created_by_id]]
+      :where [:= :u.auth0_id requester-auth0-id]})})
 
 ;; (defresolver invited-gift-lists-resolver [{:keys [db requester-auth0-id]} _]
 ;;   {::pc/output [{:invited-gift-lists [::gift-list/id]}]}
@@ -87,7 +60,7 @@
 
 (def gift-list-resolvers
   [gift-list-by-id-resolver
-   ;; created-gift-lists-resolver
+   created-gift-lists-resolver
    ;; invited-gift-lists-resolver
    ])
 
