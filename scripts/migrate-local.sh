5c433b3fd97d246fcdb415d40bf5d1555c69afd3
Chris O'Donnell <chris.bowdoin@gmail.com>
2020-02-01 10:06:14 -0500

    Update migrate-local.sh to work with new flyway image.

--- scripts/migrate-local.sh
+++ scripts/migrate-local.sh
@@ -2,6 +2,17 @@
 
 pushd "$(git rev-parse --show-toplevel)"
 
-docker run --rm --network mygiftlist_default -e POSTGRES_HOSTNAME=postgres -e POSTGRES_PORT=5432 -e POSTGRES_DB=postgres -v "$(pwd)/db_user.txt:/run/secrets/db_user" -v "$(pwd)/db_password.txt:/run/secrets/db_password" -v "$(pwd)/migrations:/flyway/sql" cpodonnell/mygiftlistrocks:migrate-latest
+docker build -f Dockerfile.flyway -t cpodonnell/mygiftlistrocks:migrate-local .
+
+docker run --rm \
+       --network mygiftlist_default \
+       -e POSTGRES_USER_FILE=/db/secrets/db_user \
+       -e POSTGRES_PASSWORD_FILE=/db/secrets/db_password \
+       -e POSTGRES_HOSTNAME=postgres \
+       -e POSTGRES_PORT=5432 \
+       -e POSTGRES_DB=postgres \
+       -v "$(pwd)/db_user.txt:/db/secrets/db_user" \
+       -v "$(pwd)/db_password.txt:/db/secrets/db_password" \
+       cpodonnell/mygiftlistrocks:migrate-local
 
 popd
